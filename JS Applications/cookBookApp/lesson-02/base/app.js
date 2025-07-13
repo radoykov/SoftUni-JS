544769bcec97045475b85abf568af962b113b093
radoykov <alex.radoykov@gmail.com>
2025-07-13 16:05:28 +0300

    Complete CookBook part 02 with improving e() func, also exporting better e func. as global in other file.

---
+++ JS Applications/cookBookApp/lesson-02/base/app.js
@@ -0,0 +1,79 @@
+import e from '../../../funcCreateElement/funcCreateElement.js'
+
+window.addEventListener('load', async () => {
+    loadPreview();
+});
+
+
+async function loadPreview() {
+    const main = document.querySelector('main');
+    const url = `http://localhost:3030/jsonstore/cookbook/recipes`;
+
+    try {
+        const response = await fetch(url);
+        const data = await response.json();
+
+        main.innerHTML = "";
+        Object.values(data).forEach(oneData => {
+            const article = createCustomArticle(false, oneData);
+            main.appendChild(article);
+        });
+
+    } catch (err) {
+        console.log(err);
+    }
+
+}
+function createCustomArticle(isExtended, recipe) {
+    let article;
+
+    if (isExtended) {
+        article =
+            e('article', {},
+                e('h2', {}, recipe.name),
+                e('div', { className: 'band' },
+                    e('div', { className: 'thumb' }, e('img', { src: recipe.img })),
+                    e('div', { className: 'ingredients' },
+                        e('h3', {}, 'Ingredients:'),
+                        e('ul', {}, recipe.ingredients.map(i => e('li', {}, i))),
+                    )
+                ),
+                e('div', { className: 'description' },
+                    e('h3', {}, 'Preparation:'),
+                    recipe.steps.map(s => e('p', {}, s))
+                ),
+            );
+    } else {
+        article =
+            e('article', { className: "preview", onClick: onLoad },
+                e('div', { className: "title" },
+                    e('h2', {}, recipe.name)),
+                e('div', { className: "small" },
+                    e('img', { src: recipe.img })));
+    }
+
+    return article;
+
+    async function onLoad(ev) {
+        const main = document.querySelector('main');
+        const url = `http://localhost:3030/jsonstore/cookbook/details/${recipe._id}`;
+
+        try {
+            const response = await fetch(url);
+            const details = await response.json();
+
+            let currArticle = ev.target;
+            while (currArticle && currArticle.tagName !== 'ARTICLE') {
+                currArticle = currArticle.parentNode;
+            }
+
+            const newArticle = createCustomArticle(true, details);
+            const myIndex = [...main.children].findIndex(el => el == currArticle);
+            main.insertBefore(newArticle, main.children[myIndex]);
+            currArticle.remove();
+
+        } catch (err) {
+            console.log(err);
+        }
+    }
+}
--- JS Applications/funcCreateElement/funcCreateElement.js
+++ JS Applications/funcCreateElement/funcCreateElement.js
@@ -1,13 +1,15 @@
-function e(type, attributes = {}, ...content) {
+function e(type, attributes, ...content) {
     const result = document.createElement(type);
 
-    for (let attr in attributes) {
+    for (let [attr, value] of Object.entries(attributes || {})) {
         if (attr.substring(0, 2) == 'on') {
-            result.addEventListener(attr.substring(2).toLowerCase(), attributes[attr]);
+            result.addEventListener(attr.substring(2).toLocaleLowerCase(), value);
         } else {
-            result[attr] = attributes[attr];
+            result[attr] = value;
         }
     }
+
+    content = content.reduce((a, c) => a.concat(Array.isArray(c) ? c : [c]), []);
 
     content.forEach(e => {
         if (typeof e == 'string' || typeof e == 'number') {
