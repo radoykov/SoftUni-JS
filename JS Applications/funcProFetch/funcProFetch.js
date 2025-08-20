7b54f6d5c916d12d619b712a24e7178b6ce3bee4
radoykov <alex.radoykov@gmail.com>
2025-08-20 16:24:48 +0300

    Making profetch global.

---
+++ JS Applications/funcProFetch/funcProFetch.js
@@ -0,0 +1,27 @@
+async function proFetch(url, method = 'get', body = {}, token = null, isItLogout = false) {
+    const headers = {
+        'Content-Type': 'application/json',
+    }
+    if (token != null && token != undefined) {
+        headers['X-Authorization'] = token;
+    }
+    const obj = {
+        method: method,
+        headers: headers,
+    };
+    if (Object.keys(body).length > 0) {
+        obj['body'] = JSON.stringify(body);
+    }
+    const response = await fetch(url, obj);
+    if (response.ok == false) {
+        alert(`Error in fetch : ${response.statusText}`);
+        return;
+    }
+    if (isItLogout) {
+        return;
+    }
+    const data = await response.json();
+    return data;
+}
+
+export default proFetch;
